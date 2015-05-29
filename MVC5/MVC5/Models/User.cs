e3266445e06d69840a22d97bdd40da37b00a3293
bhanu499 <bhanu499@gmail.com>
2015-05-30 00:00:29 +0530

    My Work

---
+++ MVC5/MVC5/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC5.Models
+{
+    public class User
+    {
+        [Required]
+        
+        [Display(Name="User Name:")]
+        public string username { get; set; }
+        [Required]
+        [Compare("username", ErrorMessage = "username and pwd hsould match")]
+        [Display(Name="Password:")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+        [Required]
+        [Display(Name="Email Id:")][DataType(DataType.EmailAddress)]
+        public string mailid { get; set; }
+    }
+}
